9c2a3a125815e09d9b3f8b4d32ed066faf1ece36
ArlifTagaree <liffism9@gmail.com>
2023-03-14 16:08:19 +0700

    Commit JS-02 Declare and Use Functions

--- app.js
+++ app.js
@@ -27,3 +27,31 @@ function prinsOut (name1,name2,name3,age1,age2,age3) {
     return (`Welcome ${name1}, you are ${age1}. Welcome ${name2}, you are ${age2}. Welcome ${name3}, you are ${age3}.`)
 }
 console.log(prinsOut ('A','B','C',30,31,32)); 
+
+//Challenge Yourself
+function min(score) {
+    if (score >= 5) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function mid(score) {
+    if (score >= 8) {
+        return 'Excellent';
+    } else {
+        return 'Try more';
+    }
+    
+}
+
+function max(score) {
+    if (score === 11) {
+        return 'Perfect';
+    } else {
+        return 'Good but not the best';
+    }
+    
+}
